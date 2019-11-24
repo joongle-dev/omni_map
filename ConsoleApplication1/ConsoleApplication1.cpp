d708a0b764f8eb50901890418a5d0b60c546d5c0
Joongho <39954597+Wulfhart@users.noreply.github.com>
2019-11-23 22:16:25 -0500

    erase function test

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -15,8 +15,16 @@ int main()
 	auto it1 = test_map.find<1>("seven");
 	std::cout << std::get<0>(*it0) << " " << std::get<1>(*it0) << std::endl;
 	std::cout << std::get<0>(*it1) << " " << std::get<1>(*it1) << std::endl;
-	test_map.erase(it0);
-	it0 = test_map.find<0>(4);
-	it1 = test_map.find<1>("four");
-	std::cout << (it0 == test_map.end<0>()) << std::endl;
+	test_map.emplace(25, "twenty-five");
+	test_map.emplace(16, "sixteen");
+	test_map.emplace(18, "eighteen");
+	test_map.emplace(17, "seventeen");
+	test_map.emplace(23, "twenty-three");
+	test_map.emplace(21, "twenty_one");
+	test_map.emplace(21, "twenty_one(1)");
+	test_map.erase(test_map.find<0>(25));
+	it0 = test_map.find<0>(21);
+	it1 = test_map.find<1>("sixteen");
+	std::cout << std::get<0>(*it0) << " " << std::get<1>(*it0) << std::endl;
+	std::cout << std::get<0>(*it1) << " " << std::get<1>(*it1) << std::endl;
 }
