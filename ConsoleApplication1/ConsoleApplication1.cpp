ce60ace353486f5604feb1b5e985266e60ce8303
Joongho <39954597+Wulfhart@users.noreply.github.com>
2019-11-23 22:20:42 -0500

    iterator compare operator test

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,6 +4,7 @@
 int main()
 {
 	util::omni_map<int, std::string> test_map;
+
 	test_map.emplace(3, "three");
 	test_map.emplace(2, "two");
 	test_map.emplace(7, "seven");
@@ -11,10 +12,12 @@ int main()
 	test_map.emplace(6, "six");
 	test_map.emplace(4, "four");
 	test_map.emplace(5, "five");
+
 	auto it0 = test_map.find<0>(4);
 	auto it1 = test_map.find<1>("seven");
 	std::cout << std::get<0>(*it0) << " " << std::get<1>(*it0) << std::endl;
 	std::cout << std::get<0>(*it1) << " " << std::get<1>(*it1) << std::endl;
+
 	test_map.emplace(25, "twenty-five");
 	test_map.emplace(16, "sixteen");
 	test_map.emplace(18, "eighteen");
@@ -22,7 +25,13 @@ int main()
 	test_map.emplace(23, "twenty-three");
 	test_map.emplace(21, "twenty_one");
 	test_map.emplace(21, "twenty_one(1)");
-	test_map.erase(test_map.find<0>(25));
+
+	it0 = test_map.find<0>(25);
+	std::cout << (it0 == test_map.end<0>() ? "true" : "false") << std::endl;
+	test_map.erase(it0);
+	it0 = test_map.find<0>(25);
+	std::cout << (it0 == test_map.end<0>() ? "true" : "false") << std::endl;
+
 	it0 = test_map.find<0>(21);
 	it1 = test_map.find<1>("sixteen");
 	std::cout << std::get<0>(*it0) << " " << std::get<1>(*it0) << std::endl;
